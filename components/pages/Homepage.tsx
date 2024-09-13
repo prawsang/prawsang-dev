128bf30c141b3d47ce4aea6e70cdd7e7a34a61c9
Prawsang Chayakulkeeree <prawsang.c@gmail.com>
2024-09-13 23:48:37 +0700

    init v2

--- components/pages/Homepage.tsx
+++ components/pages/Homepage.tsx
@@ -1,21 +1,5 @@
 import Hero from "@/components/home/Hero";
-import Intro from "../home/Intro";
-import Skills from "../home/Skills";
-import Experience from "../home/Experience";
-import Contact from "../home/Contact";
-import Footer from "../Footer";
 
 export default function Homepage() {
-  return (
-    <div className="text-base-400">
-      <Hero />
-      <div className="content">
-        <Intro />
-        <Skills />
-        <Experience />
-        <Contact />
-        <Footer />
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
